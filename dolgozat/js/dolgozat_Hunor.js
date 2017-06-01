285ca9a98812702cbde82ebee0392916201c30f3
bhunor456 <balazs.hunoristvan@gmail.com>
2017-06-01 02:03:48 -0500

    doga

---
+++ dolgozat/js/dolgozat_Hunor.js
@@ -0,0 +1,103 @@
+'use strict'
+
+class Car {
+	if (!this color)  {
+		console.log("white "+color);
+	} else {
+		value: white,
+	}
+	constructor(name, color) {
+		this.tank= 70
+		this.rendszam= "hf234"
+		this.fogyasztas= 5
+		this.passengers= []
+		this.color:"white"
+		Object.defineProperty(this, 'kerekek', {
+			value: 4,
+			writable: true,
+			enumerable: true,
+			configurable: true
+		console.log("a tank merete: 70l "+tank);
+		});
+	};
+	//printallinfo
+	console.log("hf234: "+rendszam);
+	console.log(white)
+	console.log(70)
+
+
+
+
+
+
+
+
+
+
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
